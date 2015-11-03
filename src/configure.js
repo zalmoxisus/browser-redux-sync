a02c1219b7e606e1f1d7f597eceb87d098717b15
Zalmoxisus <zalmoxisus@gmail.com>
2015-11-03 13:33:12 +0200

    Add skipRestore for redux-persist 1.4

--- src/configure.js
+++ src/configure.js
@@ -4,6 +4,7 @@ export default function configureSync(config = {}) {
   return  {
     ...config,
     storage: storage,
+    skipRestore: true,
     serialize: data => data,
     deserialize: data => data,
     debounce: 0
